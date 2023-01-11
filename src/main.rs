b5f0768a4d0cc1cedc684d02b30d359853858295
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-11 21:56:23 +0100

    Remove redundant code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use std::slice;
-
-pub mod assembler;
-pub mod concept;
-pub mod mem;
 pub mod arch;
+pub mod assembler;
+pub mod mem;
 
-fn main() {
-    let mut bytes = [0u8; 16];
-    let r = bytes.as_mut_ptr();
-    let x = unsafe { slice::from_raw_parts_mut(r, 16) };
-    x[0] = 255;
-    println!("{bytes:?}");
-}
+fn main() {}
