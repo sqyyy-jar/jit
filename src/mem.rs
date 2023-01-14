5e1aafd962a1fd064ede2fe65e63679f8a7097b9
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-14 10:22:52 +0100

    Improve memory interface

--- src/mem.rs
+++ src/mem.rs
@@ -1,6 +1,6 @@
 use std::alloc;
 
-pub fn align(size: usize, align: usize) -> usize {
+pub const fn align(size: usize, align: usize) -> usize {
     if size % align == 0 {
         size
     } else {
@@ -11,7 +11,7 @@ pub fn align(size: usize, align: usize) -> usize {
 pub fn get_system_alignment() -> usize {
     #[cfg(unix)]
     {
-        libc::sysconf(libc::_SC_PAGESIZE) as usize
+        unsafe { libc::sysconf(libc::_SC_PAGESIZE) as usize }
     }
     #[cfg(windows)]
     {
@@ -27,7 +27,7 @@ pub fn get_system_alignment() -> usize {
 pub fn alloc_aligned(size: usize) -> *mut u8 {
     unsafe {
         let alignment = get_system_alignment();
-        let size = align(size, 0);
+        let size = align(size, alignment);
         alloc::alloc_zeroed(alloc::Layout::from_size_align_unchecked(
             align(size, alignment),
             alignment,
@@ -35,16 +35,65 @@ pub fn alloc_aligned(size: usize) -> *mut u8 {
     }
 }
 
+/// # Safety
+/// This function will deallocate the given pointer without any safety checks.
+pub unsafe fn dealloc_aligned(ptr: *mut u8, size: usize) {
+    let alignment = get_system_alignment();
+    let size = align(size, alignment);
+    alloc::dealloc(
+        ptr,
+        alloc::Layout::from_size_align_unchecked(align(size, alignment), alignment),
+    );
+}
+
 pub fn make_executable(ptr: *mut u8, size: usize) -> bool {
     unsafe {
         let alignment = get_system_alignment();
-        //#[cfg(unix)]
+        #[cfg(unix)]
         {
-            libc::mprotect()
+            libc::mprotect(
+                ptr as *mut _,
+                align(size, alignment),
+                libc::PROT_READ | libc::PROT_EXEC,
+            ) == 0
         }
         #[cfg(windows)]
         {
-            todo!()
+            use windows::Win32::System::Memory;
+            let mut _old_fp = Memory::PAGE_PROTECTION_FLAGS::default();
+            Memory::VirtualProtect(
+                ptr as *mut _,
+                align(size, alignment),
+                Memory::PAGE_EXECUTE_READ,
+                &mut _old_fp,
+            )
+            .as_bool()
+        }
+    }
+}
+
+pub fn make_readwrite(ptr: *mut u8, size: usize) -> bool {
+    unsafe {
+        let alignment = get_system_alignment();
+        #[cfg(unix)]
+        {
+            libc::mprotect(
+                ptr as *mut _,
+                align(size, alignment),
+                libc::PROT_READ | libc::PROT_WRITE,
+            ) == 0
+        }
+        #[cfg(windows)]
+        {
+            use windows::Win32::System::Memory;
+            let mut _old_fp = Memory::PAGE_PROTECTION_FLAGS::default();
+            Memory::VirtualProtect(
+                ptr as *mut _,
+                align(size, alignment),
+                Memory::PAGE_READWRITE,
+                &mut _old_fp,
+            )
+            .as_bool()
         }
     }
 }
