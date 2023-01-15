ef0445a5f70de825512c0f2a06d0d6e74cf80832
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-15 13:20:06 +0100

    Add VTable

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,11 +1,12 @@
+use crate::mem;
 use std::collections::HashMap;
 
 pub trait Assembler {
     type AsmRoutine: Subroutine;
 
-    fn get_label_address(&self, name: &str) -> usize;
+    fn get_label_address(&self, label: &str) -> usize;
 
-    fn jit(self) -> HashMap<String, fn()>;
+    fn jit(self) -> Option<VTable>;
 }
 
 pub trait Subroutine {
@@ -16,4 +17,31 @@ pub trait Subroutine {
 
 pub trait PostOp {
     fn process(&self, assembler: &impl Assembler, abs_addrs: usize, bytes: &mut [u8]);
+}
+
+pub struct VTable {
+    ptr: *mut u8,
+    size: usize,
+    table: HashMap<String, fn()>,
+}
+
+impl VTable {
+    pub fn new(ptr: *mut u8, size: usize, table: HashMap<String, fn()>) -> Self {
+        Self { ptr, size, table }
+    }
+
+    pub fn lookup(&self, label: &str) -> Option<fn()> {
+        self.table.get(label).cloned()
+    }
+}
+
+impl Drop for VTable {
+    fn drop(&mut self) {
+        if !mem::make_readwrite_aligned(self.ptr, self.size) {
+            panic!("Could not drop VTable")
+        }
+        unsafe {
+            mem::dealloc_aligned(self.ptr, self.size);
+        }
+    }
 }
