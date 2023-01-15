710111dcc11d37cfe77a160b558ab1e688e51b20
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-15 15:57:17 +0100

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 use arch::a64::{asm::Asm, reg::Reg, routine::Routine};
 use assembler::Assembler;
-use std::{
-    env::args,
-    fs::File,
-    io::{Result, Write},
-};
+use std::io::Result;
 
 pub mod arch;
 pub mod assembler;
 pub mod mem;
 
 fn main() -> Result<()> {
-    let mut f = File::create(args().nth(1).unwrap())?;
+    //let mut f = File::create(args().nth(1).unwrap())?;
     let mut asm = Asm::default();
 
     let mut main = Routine::new("main".to_string());
-    main.mov_imm16(Reg::X10, 0xCAFE);
-    main.br_link("test".to_string());
+    //main.mov_imm16(Reg::X10, 0xCAFE);
+    //main.br_link("test".to_string());
     main.ret();
     asm.push_routine(main);
 
@@ -26,8 +22,10 @@ fn main() -> Result<()> {
     test.ret();
     asm.push_routine(test);
 
-    let (code, vtable) = asm.virtual_jit().unwrap();
-    f.write_all(&code)?;
-    println!("VTable: {vtable:#?}");
+    let vtable = asm.jit().unwrap();
+    vtable.lookup("main").unwrap()();
+    // let (code, vtable) = asm.virtual_jit().unwrap();
+    // f.write_all(&code)?;
+    // println!("VTable: {vtable:#?}");
     Ok(())
 }
